595deee4e2bce911e4dc6e3735f5c6008e6d6419
Evelyn So <158375673+EvelynMinkyoungSo@users.noreply.github.com>
2025-12-04 12:15:20 -0800

    Delete test_env.py

--- test_env.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-print("URI:", os.getenv("NEO4J_URI"))
-print("USER:", os.getenv("NEO4J_USERNAME"))
-print("PASS:", os.getenv("NEO4J_PASSWORD"))
